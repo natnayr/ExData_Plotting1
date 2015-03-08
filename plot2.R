6b533651c2cad3e23a168ab6dfc5e3b13fade3b4
Ryan Tan <ryantanjunming@gmail.com>
2015-03-09 05:06:58 +0700

    Completed Plot1, Plot2, Plot3 with R files and resulting images

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 #Read FIles
 data_all <- read.table(file="exdata-data-household_power_consumption/household_power_consumption.txt", sep=";", header=TRUE,na.strings="?", 
-                   nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 
 #Format Date
 data_all$Date <- as.Date(data_all$Date, format = "%d/%m/%Y")
@@ -12,9 +12,9 @@ rm(data_all)
 #Join DateTime columns
 data$Datetime <- as.POSIXct(paste(as.Date(data$Date), data$Time))
 
-#Create red histogram
-hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+#Create line chart
+plot(x=data$Datetime, y=data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
 #Copy and create png file
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 #Read FIles
 data_all <- read.table(file="exdata-data-household_power_consumption/household_power_consumption.txt", sep=";", header=TRUE,na.strings="?", 
-                   nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 
 #Format Date
 data_all$Date <- as.Date(data_all$Date, format = "%d/%m/%Y")
@@ -12,9 +12,13 @@ rm(data_all)
 #Join DateTime columns
 data$Datetime <- as.POSIXct(paste(as.Date(data$Date), data$Time))
 
-#Create red histogram
-hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red")
+with(data, {
+  plot(Sub_metering_1~Datetime, type="l", xlab="", ylab="Energy sub metering")
+  lines(Sub_metering_2~Datetime, col="red")
+  lines(Sub_metering_3~Datetime, col="blue")
+})
+
 
 #Copy and create png file
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
